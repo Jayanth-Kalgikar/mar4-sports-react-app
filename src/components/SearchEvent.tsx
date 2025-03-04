07dadadad2814153be76e395793b90ad508a2025
Jayanth Kalgikar <jayanthkalgikar2005@gmail.com>
2025-03-04 10:46:32 +0530

    v2

--- src/components/SearchEvent.tsx
+++ src/components/SearchEvent.tsx
@@ -51,18 +51,7 @@ export default function SearchEvent() {
       <td>Otto</td>
       <td>@mdo</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
+   </tbody>
 </table>
 
       </div></div>
